4d515cf80f802429dd230b263aa54be4ba19dcb2
Alexander Groß <agross@therightstuff.de>
2011-08-04 12:26:07 +0200

    Adding IIS Express and setting it up for WatiN web specs

--- WebSpecs-Watin.cmd
+++ WebSpecs-Watin.cmd
@@ -1,4 +1,5 @@
 @echo off
-rem Please compile and start the web application with Visual Studio first.
+rem Please compile the web application with Visual Studio first.
 @mkdir _Report 2> NUL
+
 packages\Machine.Specifications.0.4.10.0\tools\mspec-clr4.exe --html _Report\WebSpecs-Watin.html "WebSpecs\LoginApp.Watin.Specs\bin\Debug\LoginApp.Watin.Specs.dll"
---
+++ WebSpecs/LoginApp.Watin.Specs/IisExpress.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+
+using Machine.Specifications;
+
+namespace LoginApp.Specs.Watin
+{
+	public class IisExpress : IAssemblyContext
+	{
+		Process _iisExpress;
+
+		public void OnAssemblyStart()
+		{
+			var iis = Path.Combine(Path.GetFullPath(ConfigurationManager.AppSettings["PathToIisExpress"]), "iisexpress.exe");
+			var app = Path.GetFullPath(ConfigurationManager.AppSettings["PathToWebApp"]);
+
+			_iisExpress = new Process();
+			var psi = new ProcessStartInfo
+			          {
+			          	FileName = iis,
+			          	Arguments = String.Format(@"/path:""{0}"" /port:1337", app)
+			          };
+
+			_iisExpress.StartInfo = psi;
+			_iisExpress.Start();
+		}
+
+		public void OnAssemblyComplete()
+		{
+			_iisExpress.Kill();
+		}
+	}
+}
